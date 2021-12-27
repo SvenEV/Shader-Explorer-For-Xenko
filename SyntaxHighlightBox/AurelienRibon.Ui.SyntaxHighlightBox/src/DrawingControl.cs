c04fa653d7c8cecde6631f4afb5bc97479a2a267
tebjan <tebjan@vvvv.org>
2021-12-27 16:12:42 +0100

    Changes font to Consolas and code auto formatting

--- SyntaxHighlightBox/AurelienRibon.Ui.SyntaxHighlightBox/src/DrawingControl.cs
+++ SyntaxHighlightBox/AurelienRibon.Ui.SyntaxHighlightBox/src/DrawingControl.cs
@@ -4,25 +4,30 @@ using System.Windows.Media;
 
 namespace AurelienRibon.Ui.SyntaxHighlightBox
 {
-    public class DrawingControl : FrameworkElement {
+    public class DrawingControl : FrameworkElement
+    {
         private VisualCollection visuals;
         private DrawingVisual visual;
 
-        public DrawingControl() {
+        public DrawingControl()
+        {
             visual = new DrawingVisual();
             visuals = new VisualCollection(this);
             visuals.Add(visual);
         }
 
-        public DrawingContext GetContext() {
+        public DrawingContext GetContext()
+        {
             return visual.RenderOpen();
         }
 
-        protected override int VisualChildrenCount {
+        protected override int VisualChildrenCount
+        {
             get { return visuals.Count; }
         }
 
-        protected override Visual GetVisualChild(int index) {
+        protected override Visual GetVisualChild(int index)
+        {
             if (index < 0 || index >= visuals.Count)
                 throw new ArgumentOutOfRangeException();
             return visuals[index];
